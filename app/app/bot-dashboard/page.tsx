c599a6d1d6d3f86a31f046483409ea569384f983
danyilchukhrai <chukhraidanyil@gmail.com>
2023-12-30 00:00:58 +0900

    removed bot-dashboard

--- app/app/bot-dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import dynamic from 'next/dynamic';
-
-const BotDashboard = dynamic(() => import('@/containers/bot-dashboard'));
-
-export default function BotDashboardPage() {
-  return <BotDashboard />;
-}
